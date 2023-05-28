8e952b36f76edcc2c4c0e7c57d7d3276af9fd555
0verfl0w767 <tkddbs7631@naver.com>
2023-05-28 15:23:40 +0900

    moved repository from @0verfl0w767 to @syu-kr

---
+++ index.js
@@ -0,0 +1,123 @@
+/**
+ *    ____                  __________
+ *   / __ \_   _____  _____/ __/ / __ \_      __
+ *  / / / / | / / _ \/ ___/ /_/ / / / / | /| / /
+ * / /_/ /| |/ /  __/ /  / __/ / /_/ /| |/ |/ /
+ * \____/ |___/\___/_/  /_/ /_/\____/ |__/|__/
+ *
+ * The copyright indication and this authorization indication shall be
+ * recorded in all copies or in important parts of the Software.
+ *
+ * @author 0verfl0w767
+ * @link https://github.com/0verfl0w767
+ * @license MIT LICENSE
+ *
+ */
+const express = require('express')
+const {param, validationResult} = require('express-validator')
+const http = require('http')
+const fs = require('fs')
+const requestIp = require('request-ip')
+
+const app = express()
+
+const date = () => {
+  const today = new Date()
+  const year = today.getFullYear()
+  const month = ('0' + (today.getMonth() + 1)).slice(-2)
+  const day = ('0' + today.getDate()).slice(-2)
+  return year + '-' + month + '-' + day
+}
+
+const time = () => {
+  const today = new Date()
+  const hours = ('0' + today.getHours()).slice(-2)
+  const minutes = ('0' + today.getMinutes()).slice(-2)
+  const seconds = ('0' + today.getSeconds()).slice(-2)
+  return hours + ':' + minutes + ':' + seconds
+}
+
+const prefix = () => {
+  return '[' + date() + ' ' + time() + ']'
+}
+
+const userInfo = (req) => {
+  console.log(prefix() + ' client IP: ' + requestIp.getClientIp(req))
+  console.log(prefix() + ' url: ' + req.originalUrl)
+}
+
+app.use('/', express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  userInfo(req)
+  res.status(200).sendFile(__dirname + '/page/index.html')
+})
+
+app.get('/department', (req, res) => {
+  userInfo(req)
+  res.status(200).sendFile(__dirname + '/page/department.html')
+})
+
+app.get('/info', (req, res) => {
+  userInfo(req)
+  res.status(200).sendFile(__dirname + '/page/info.html')
+})
+
+app.get('/api/docs', (req, res) => {
+  userInfo(req)
+  res.status(200).sendFile(__dirname + '/page/api/docs.html')
+})
+
+app.get('/api/college/v1/all', (req, res) => {
+  userInfo(req)
+  const jsonData = JSON.parse(fs.readFileSync(__dirname + '/data/학부(과).json', 'utf8'))
+  res.status(200).json(jsonData)
+})
+
+app.get(
+  '/api/undergraduate/v1/:year/:semester/:id',
+  param('year').exists(),
+  param('year').isInt({min: 2000, max: 2023}),
+  param('semester').exists(),
+  param('semester').isInt({min: 1, max: 2}),
+  param('id').exists(),
+  param('id').isInt({min: 1, max: 63}),
+  (req, res) => {
+    userInfo(req)
+    const errorFormatter = ({location, msg, param, value, nestedErrors}) => {
+      return param + ': ' + msg
+    }
+    const result = validationResult(req).formatWith(errorFormatter)
+    if (!result.isEmpty()) {
+      res.status(401).json(result)
+      return
+    }
+    const year = req.params.year
+    const semester = req.params.semester
+    const convertSemester = {1: '1학기 정규', 2: '2학기 정규'}
+    const id = req.params.id
+    const dirPath = __dirname + '/data/' + year + '/' + convertSemester[semester]
+    const undergraduates = JSON.parse(fs.readFileSync(dirPath + '/학부(과).json', 'utf8'))
+    let undergraduateData = null
+    for (const index in undergraduates['api']) {
+      if (undergraduates['api'][index]['식별번호'] == id) {
+        undergraduateData = undergraduates['api'][index]
+      }
+    }
+    if (undergraduateData === null) {
+      res.status(401).json({statusCode: 401, message: 'unknown request.'})
+      return
+    }
+    const jsonData = JSON.parse(
+      fs.readFileSync(dirPath + '/전체대학/' + undergraduateData['학부(과)'] + '.json', 'utf8'),
+    )
+    res.status(200).json(jsonData)
+  },
+)
+
+app.get('*', (req, res) => {
+  userInfo(req)
+  res.status(404).json({statusCode: 404, message: 'unknown request.'})
+})
+
+http.createServer(app).listen(1235, '0.0.0.0')
---
+++ public/js/main.js
@@ -0,0 +1,162 @@
+let jsondata = {}
+let daySelectCount = 0
+
+async function getRequest() {
+  const getResponse = await fetch('https://lecture.syu.kr/api/undergraduate/v1/2023/1/15', {
+    method: 'get',
+  })
+  const getJson = await getResponse.json()
+  return getJson
+}
+
+getRequest().then((data) => {
+  setBasePage(data, '전체')
+  jsondata = data
+})
+
+function daySelectEvent(object) {
+  daySelectCount = 0
+  setBasePage(jsondata, object.value)
+}
+
+function setBasePage(datas, day) {
+  let classArea = [
+    '기초교양',
+    '인성교양',
+    '일반선택영역',
+    '자연과학영역',
+    '사회과학영역',
+    '인문예술영역',
+    '디지털 리터러시영역',
+    '', // 교직영역
+  ]
+  for (let i = 0; i < classArea.length; i++) {
+    const sectionCount = String(i + 1) // 1, 2, 3, 4, 5, 6, 7, 8
+    const sectionInfo = String(i + 1) + String(i + 1) // 11, 22, 33, 44, 55, 66, 77, 88
+    document.getElementById(sectionCount).innerHTML =
+      getSectionCount(datas, day, classArea[i]) + '개'
+    document.getElementById(sectionInfo).innerHTML = getSectionInfo(datas, day, classArea[i])
+  }
+  document.getElementById('count').innerHTML = daySelectCount + '개'
+}
+
+function getSectionCount(datas, day, className) {
+  let count = 0
+  for (let i = 0; i < datas['api'].length; i++) {
+    if (datas['api'][i]['영역구분'] === className) {
+      if (day === '전체') {
+        count++
+        daySelectCount++
+      } else if (datas['api'][i]['수업시간'].substr(0, 1) === day) {
+        count++
+        daySelectCount++
+      } else if (datas['api'][i]['수업시간'].split(',')[1] !== undefined) {
+        if (datas['api'][i]['수업시간'].split(',')[1].substr(0, 1) === day) {
+          count++
+          daySelectCount++
+        }
+      }
+    }
+  }
+  return count
+}
+
+function convertDay(day) {
+  let dayNum = 0
+  if (day === '월') dayNum = 1
+  else if (day === '화') dayNum = 2
+  else if (day === '수') dayNum = 3
+  else if (day === '목') dayNum = 4
+  else if (day === '금') dayNum = 5
+  else dayNum = 0
+  return dayNum
+}
+
+function starTest() {
+  let stars = ''
+  for (let i = 0; i < 5; i++) {
+    // stars += "<i class=\"bi bi-star\"></i>"
+    stars += '<i class="bi bi-star-fill" style="color: yellow;"></i>'
+  }
+  return stars
+}
+
+function getSectionInfo(datas, day, className) {
+  let newData = []
+  let html_tag = ''
+  for (let i = 0; i < datas['api'].length; i++) {
+    if (datas['api'][i]['영역구분'] === className) {
+      if (day === '전체') {
+        newData.push(datas['api'][i])
+      } else if (datas['api'][i]['수업시간'].substr(0, 1) === day) {
+        newData.push(datas['api'][i])
+      } else if (datas['api'][i]['수업시간'].split(',')[1] !== undefined) {
+        if (datas['api'][i]['수업시간'].split(',')[1].substr(0, 1) === day) {
+          newData.push(datas['api'][i])
+        }
+      }
+    }
+  }
+  newData.sort((a, b) => {
+    const dayA = a['수업시간'].substr(0, 1)
+    const dayB = b['수업시간'].substr(0, 1)
+    if (convertDay(dayA) > convertDay(dayB)) return 1
+    if (convertDay(dayA) < convertDay(dayB)) return -1
+    const timeA = parseInt(a['수업시간'].substr(1, 2))
+    const timeB = parseInt(b['수업시간'].substr(1, 2))
+    if (timeA > timeB) return 1
+    if (timeA < timeB) return -1
+  })
+  for (let i = 0; i < newData.length; i++) {
+    html_tag += `
+      <tr>
+        <td nowrap><span style="color: #5f6062;">${newData[i]['과목코드']}</span></td>
+        <td nowrap><strong><span style="color: white;">${newData[i]['과목명']}</span></strong></td>
+        <td nowrap><span style="color: #5f6062;">${newData[i]['학년']}</span></td>
+        <td nowrap><span style="color: yellow;">${newData[i]['학점']}</span></td>
+        <td nowrap><span style="color: #5f6062;">${newData[i]['이수구분']}</span></td>
+        <td nowrap><span style="color: #5f6062;">
+          <a href="https://everytime.kr/lecture/search?keyword=${
+            newData[i]['교수명']
+          }&condition=professor" target="_blank">
+            ${newData[i]['교수명']}
+          </a>
+        </span></td>
+        <td nowrap><span style="color: yellow;">${newData[i]['수업시간']}</span></td>
+        <td class="text-end" nowrap>
+          <span style="color: #5f6062;">
+            ${newData[i]['장소'].replace(/강의실|\(小\)|\(中\)|\(大\)/g, '')}
+          </span>
+        </td>
+        <td class="text-center" nowrap>
+          <span style="color: #5f6062;">
+            ${starTest()}
+          </span>
+        </td>
+      </tr>
+    `
+  }
+  let table_tag = `
+    <div class="table-responsive">
+      <table class="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col" nowrap>과목코드</th>
+            <th scope="col" nowrap>과목명</th>
+            <th scope="col" nowrap>학년</th>
+            <th scope="col" nowrap>학점</th>
+            <th scope="col" nowrap>이수구분</th>
+            <th scope="col" nowrap>교수명</th>
+            <th scope="col" nowrap>수업시간
+            <th class="text-end" scope="col" nowrap>장소</th>
+            <th class="text-center" scope="col" nowrap>별점</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${html_tag}
+        </tbody>
+      </table>
+    </div>
+  `
+  return table_tag
+}
